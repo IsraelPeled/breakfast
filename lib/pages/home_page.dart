819f084fb07b5e117ff7513ae2719df5fe5fc6d8
IsraelPeled <ipb14ck0ut@gmail.com>
2024-09-21 21:03:47 +0300

    add categories

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,76 +1,103 @@
+import 'package:breakfast/components/search_field.dart';
+import 'package:breakfast/models/category_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<CategoryModel> categories = [];
+
+  void _getCategoris(){
+    categories = CategoryModel.getCategories();
+  }
+
+  @override
+  void initState(){
+    _getCategoris();
+  }
 
   @override
   Widget build(BuildContext context) {
+    _getCategoris();
     return Scaffold(
       appBar: appBar(),
       backgroundColor: Colors.white,
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          _searchField(),
+          searchField(),
+          SizedBox(height: 40,),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: Text(
+                  'Category',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600
+                  ),
+                ),
+              ),
+              SizedBox(height: 15,),
+              Container(
+                height: 120,
+                child: ListView.separated(
+                  itemCount: categories.length,
+                    scrollDirection: Axis.horizontal,
+                    padding: EdgeInsets.only(left: 20, right: 20),
+                    separatorBuilder: (context, index) => SizedBox(width: 25,),
+                    itemBuilder: (context, index){
+                      return Container(
+                        width: 50,
+                        decoration: BoxDecoration(
+                          color: categories[index].boxColor.withOpacity(0.3),
+                          borderRadius: BorderRadius.circular(16)
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Container(
+                              width: 50,
+                              height: 50,
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  shape: BoxShape.circle
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: SvgPicture.asset(categories[index].iconPath),
+                              ),
+                            ),
+                            Text(
+                              categories[index].name,
+                              style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                                color: Colors.black,
+                                fontSize: 14,
+                              ),
+                            )
+                          ],
+                        ),
+                      );
+                    }
+                ),
+              ),
+            ],
+          )
         ],
       ),
     );
   }
-}
-
-
-Container _searchField(){
-   return Container(
-     margin: const EdgeInsets.only(top: 40, left: 20, right: 20),
-     decoration: BoxDecoration(
-         boxShadow: [BoxShadow(
-             color: Color(0xff101617).withOpacity(0.11),
-             blurRadius: 40,
-             spreadRadius: 0
-         )]
-     ),
-     child: TextField(
-       decoration: InputDecoration(
-           filled: true,
-           fillColor: Colors.white,
-           contentPadding: const EdgeInsets.all(15),
-           hintText: 'Search Pancake',
-           hintStyle: TextStyle(
-               color: Color(0xffdddada),
-               fontSize: 14
-           ),
-           prefixIcon: Padding(
-             padding: const EdgeInsets.all(12),
-             child: SvgPicture.asset('assets/icons/Search.svg'),
-           ),
-           suffixIcon: Container(
-             width: 100,
-             child: IntrinsicHeight(
-               child: Row(
-                 mainAxisAlignment: MainAxisAlignment.end,
-                 children: [
-                   VerticalDivider(
-                     color: Colors.black,
-                     indent: 10,
-                     endIndent: 10,
-                     thickness: 0.1,
-                   ),
-                   Padding(
-                     padding: const EdgeInsets.all(8.0),
-                     child: SvgPicture.asset('assets/icons/Filter.svg'),
-                   ),
-                 ],
-               ),
-             ),
-           ),
-           border: OutlineInputBorder(
-               borderRadius: BorderRadius.circular(15),
-               borderSide: BorderSide.none
-           )
-       ),
-     ),
-   );
 }
 
 AppBar appBar(){
